27b2bbb3e0c01c82b89a389d1fe81f8d425132c2
Soren van Deinse <svandeinse@gmail.com>
2016-06-15 02:39:44 +0000

    adding express getting started

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log('hello world!');
 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
